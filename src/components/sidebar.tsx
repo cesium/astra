e7fc38d7c0436988d662837ac7a706adf368ed2d
11GG20 <fl11gg20@gmail.com>
2025-07-05 14:52:28 +0100

    fix: ran format

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
-import Link from 'next/link';
-import { useState, createContext, useContext } from 'react';
+import Link from "next/link";
+import { useState, createContext, useContext } from "react";
 
 interface ParentProps {
-  children: React.ReactNode
-  className?: string
+  children: React.ReactNode;
+  className?: string;
 }
 
 interface SideBarProps extends ParentProps {
@@ -18,63 +18,61 @@ interface SideBarContextType {
 }
 
 interface ItemProps extends ParentProps {
-  id: string
-  href: string
-} 
+  id: string;
+  href: string;
+}
 
 // Compound Components
 
 const SideBarContext = createContext<SideBarContextType | null>(null);
 
-
-export function SibebarHeader({children, className}: ParentProps) {
+export function SibebarHeader({ children, className }: ParentProps) {
   return (
-    <div className={`pt-5 pb-3 mx-3 mb-2.5 border-b border-gray-200 text-[#00000080] ${className}`}>
+    <div
+      className={`mx-3 mb-2.5 border-b border-gray-200 pt-5 pb-3 text-[#00000080] ${className}`}
+    >
       {children}
     </div>
-  )
+  );
 }
 
-export function SidebarItemList({children, className}: ParentProps) {
-  return (
-    <div className={`flex flex-col gap-2 ${className}`}>
-      {children}
-    </div>
-  )
+export function SidebarItemList({ children, className }: ParentProps) {
+  return <div className={`flex flex-col gap-2 ${className}`}>{children}</div>;
 }
 
-export function SidebarItem({children, className, id, href}: ItemProps) {
-    const context = useContext(SideBarContext);
-    
-    if (!context) {
-      throw new Error('Item must be used within a Sidebar');
-    }
-    
-    const {selected, setSelected} = context;
+export function SidebarItem({ children, className, id, href }: ItemProps) {
+  const context = useContext(SideBarContext);
 
-    const isSelected = selected === id
+  if (!context) {
+    throw new Error("Item must be used within a Sidebar");
+  }
+
+  const { selected, setSelected } = context;
+
+  const isSelected = selected === id;
 
   return (
     <Link
-      href={href} 
+      href={href}
       onClick={() => setSelected(id)}
-      className={`inline-flex gap-2 items-center group py-2.5 px-3 text-[#00000080] rounded-lg transition-all duration-300 ease-in-out cursor-pointer ${isSelected ? "ring-1 ring-[#ee784971] bg-[#EE77491A] text-[#EE7749]" : "hover:bg-gray-100"} ${className}`}>
+      className={`group inline-flex cursor-pointer items-center gap-2 rounded-lg px-3 py-2.5 text-[#00000080] transition-all duration-300 ease-in-out ${isSelected ? "bg-[#EE77491A] text-[#EE7749] ring-1 ring-[#ee784971]" : "hover:bg-gray-100"} ${className}`}
+    >
       {children}
     </Link>
-  )
+  );
 }
-
 
 // Main Sidebar component
-export default function Sidebar({children, defaultSelected}: SideBarProps) {
-    const [selected, setSelected] = useState<string | null>(defaultSelected || null);
+export default function Sidebar({ children, defaultSelected }: SideBarProps) {
+  const [selected, setSelected] = useState<string | null>(
+    defaultSelected || null,
+  );
 
   return (
-    <div className='h-full py-2 px-0.5'>
-      <SideBarContext.Provider value={{selected, setSelected}}>
-          {children}
+    <div className="h-full px-0.5 py-2">
+      <SideBarContext.Provider value={{ selected, setSelected }}>
+        {children}
       </SideBarContext.Provider>
     </div>
-  )
+  );
 }
-
