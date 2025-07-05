e38c14a4745638c973ba5d38dbde85422c4fa228
11GG20 <fl11gg20@gmail.com>
2025-07-05 14:44:08 +0100

    fix: final touches

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,9 +1,15 @@
 'use client'
 
+import Link from 'next/link';
 import { useState, createContext, useContext } from 'react';
 
 interface ParentProps {
   children: React.ReactNode
+  className?: string
+}
+
+interface SideBarProps extends ParentProps {
+  defaultSelected?: string;
 }
 
 interface SideBarContextType {
@@ -13,6 +19,7 @@ interface SideBarContextType {
 
 interface ItemProps extends ParentProps {
   id: string
+  href: string
 } 
 
 // Compound Components
@@ -20,23 +27,23 @@ interface ItemProps extends ParentProps {
 const SideBarContext = createContext<SideBarContextType | null>(null);
 
 
-export function SibebarHeader({children}: ParentProps) {
+export function SibebarHeader({children, className}: ParentProps) {
   return (
-    <div className='pt-5 pb-3 mx-3 mb-2.5 border-b border-gray-300 text-[#00000080]'>
+    <div className={`pt-5 pb-3 mx-3 mb-2.5 border-b border-gray-200 text-[#00000080] ${className}`}>
       {children}
     </div>
   )
 }
 
-export function SidebarItemList({children}: ParentProps) {
+export function SidebarItemList({children, className}: ParentProps) {
   return (
-    <div className='flex flex-col gap-2'>
+    <div className={`flex flex-col gap-2 ${className}`}>
       {children}
     </div>
   )
 }
 
-export function SidebarItem({children, id}: ItemProps) {
+export function SidebarItem({children, className, id, href}: ItemProps) {
     const context = useContext(SideBarContext);
     
     if (!context) {
@@ -48,18 +55,19 @@ export function SidebarItem({children, id}: ItemProps) {
     const isSelected = selected === id
 
   return (
-    <div 
+    <Link
+      href={href} 
       onClick={() => setSelected(id)}
-      className={`inline-flex gap-2 items-center group py-2.5 px-3 text-[#00000080] rounded-xl transition-all duration-300 ease-in-out cursor-pointer ${isSelected ? "ring-1 ring-[#ee784971] bg-[#EE77491A] text-[#EE7749]" : "hover:bg-gray-100"}`}>
+      className={`inline-flex gap-2 items-center group py-2.5 px-3 text-[#00000080] rounded-lg transition-all duration-300 ease-in-out cursor-pointer ${isSelected ? "ring-1 ring-[#ee784971] bg-[#EE77491A] text-[#EE7749]" : "hover:bg-gray-100"} ${className}`}>
       {children}
-    </div>
+    </Link>
   )
 }
 
 
 // Main Sidebar component
-export default function Sidebar({children}: ParentProps) {
-    const [selected, setSelected] = useState<string | null>(null);
+export default function Sidebar({children, defaultSelected}: SideBarProps) {
+    const [selected, setSelected] = useState<string | null>(defaultSelected || null);
 
   return (
     <div className='h-full py-2 px-0.5'>
@@ -70,6 +78,3 @@ export default function Sidebar({children}: ParentProps) {
   )
 }
 
-// Assign compound components
-Sidebar.ItemList = SidebarItemList;
-Sidebar.Item = SidebarItem;
