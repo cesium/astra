b7c50c1d19b9a2125635207f16bb32a162324043
fp <a107009@alunos.uminho.pt>
2025-07-06 16:45:21 +0100

    improve readability

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,14 +1,13 @@
-export default function Card ({
-	children
-}: {
-	children?: React.ReactNode;
-}) {
-	return (
-		<div
-			className={'p-24 border rounded-[15px] border-[#eeeeee] bg-[#fafafa]'}
-		>
-
-			{children}
-		</div>
-	);
+interface CardProps {
+  children?: React.ReactNode;
 }
+
+function Card({ children }: CardProps) {
+  return (
+    <div className="p-24 border rounded-[15px] border-[#eeeeee] bg-[#fafafa]">
+      {children}
+    </div>
+  );
+}
+
+export default Card;
