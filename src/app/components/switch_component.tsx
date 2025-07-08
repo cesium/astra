242462204bb96046acd046ba9224edcff74bd1ea
Afonso Martins <afonsopm2005@gmail.com>
2025-07-08 23:43:27 +0100

    format

--- src/app/components/switch_component.tsx
+++ src/app/components/switch_component.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 interface IToggleSwitchProps {
   initialState?: boolean;
   onToggle?: (state: boolean) => void;
 }
 
-const ToggleSwitch: React.FC<IToggleSwitchProps> = ({ initialState = false, onToggle }) => {
+const ToggleSwitch: React.FC<IToggleSwitchProps> = ({
+  initialState = false,
+  onToggle,
+}) => {
   const [isOn, setIsOn] = useState(initialState);
 
   const handleToggle = () => {
@@ -21,19 +24,13 @@ const ToggleSwitch: React.FC<IToggleSwitchProps> = ({ initialState = false, onTo
   return (
     <button
       onClick={handleToggle}
-      className={`
-        relative inline-flex items-center h-8 w-14 rounded-full transition-colors duration-200 ease-in-out focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-orange-300
-        ${isOn ? 'bg-orange-500' : 'bg-gray-300'}
-      `}
+      className={`relative inline-flex h-8 w-14 items-center rounded-full transition-colors duration-200 ease-in-out focus:ring-1 focus:ring-orange-300 focus:ring-offset-1 focus:outline-none ${isOn ? "bg-orange-500" : "bg-gray-300"} `}
       role="switch"
       aria-checked={isOn}
     >
       <span className="sr-only">Toggle switch</span>
       <span
-        className={`
-          inline-block w-6 h-6 transform bg-white rounded-full shadow-lg transition-transform duration-200 ease-in-out
-          ${isOn ? 'translate-x-6' : 'translate-x-1'}
-        `}
+        className={`inline-block h-6 w-6 transform rounded-full bg-white shadow-lg transition-transform duration-200 ease-in-out ${isOn ? "translate-x-6" : "translate-x-1"} `}
       />
     </button>
   );
