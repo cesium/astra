c02b9aaa580207ced0ed2dd8af91d65c01d60d7c
fp <a107009@alunos.uminho.pt>
2025-07-10 18:14:06 +0100

    chore: small tweaks

--- src/stories/Card.stories.ts
+++ src/stories/Card.stories.ts
@@ -1,9 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
-import '../app/globals.css';
 import Card from '../components/Card';
 
 const meta = {
-  title: 'Card',
+  title: 'Components/Card',
   component: Card,
   parameters: {
     layout: 'centered',
