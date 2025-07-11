ca8f57c805ff4cfb5149924f807a230d054a68bb
Afonso Martins <afonsopm2005@gmail.com>
2025-07-11 22:30:53 +0100

    feat: remove comments

--- src/stories/ToggleSwitch.stories.ts
+++ src/stories/ToggleSwitch.stories.ts
@@ -4,17 +4,13 @@ import { fn } from 'storybook/test';
 
 import ToggleSwitch from '../app/components/switch_component';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Example/ToggleSwitch',
   component: ToggleSwitch,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     initialState: { control: 'boolean' },
     size: {
@@ -22,14 +18,12 @@ const meta = {
       options: ['small', 'medium', 'large'],
     },
   },
-  // Use `fn` to spy on the onToggle arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onToggle: fn() },
 } satisfies Meta<typeof ToggleSwitch>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Enabled: Story = {
   args: {
     initialState: false,
