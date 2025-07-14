78b87db49084797fb48294d11e2d5ee81a1d5739
fp <a107009@alunos.uminho.pt>
2025-07-14 18:25:11 +0100

    add className prop

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,11 @@
 interface ICardProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-function Card({ children }: ICardProps) {
+function Card({ children, className }: ICardProps) {
   return (
-    <div className="p-4 border rounded-[15px] border-[#eeeeee] bg-[#fafafa]">
+    <div className={`${className} p-4 border rounded-[15px] border-[#eeeeee] bg-[#fafafa]`}>
       {children}
     </div>
   );
