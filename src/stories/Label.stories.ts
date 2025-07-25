196bfd6aedcfe8d6f2ce263115a9db525c89deb3
Afonso Martins <afonsopm2005@gmail.com>
2025-07-25 21:47:31 +0100

    feat: update directory

--- src/stories/Label.stories.ts
+++ src/stories/Label.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 
 import { fn } from 'storybook/test';
 
-import Label from '../../components/label';
+import Label from '../components/label';
 
 const meta = {
   title: 'Components/Label',
