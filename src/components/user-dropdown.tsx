0380faae8a10da790116722f54eb9b88e24dacec
Nuno Miguel <nmpf.2005@gmail.com>
2025-08-07 17:10:25 +0100

    feat: improve user dropdown design

--- src/components/user-dropdown.tsx
+++ src/components/user-dropdown.tsx
@@ -1,29 +1,38 @@
+"use client";
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import Avatar from "./avatar";
+import Card from "./card";
+import { useState } from "react";
+import { User } from "@/lib/types";
 
 const UserDropdown = () => {
+  const [user, setUser] = useState<User>();
+
   return (
     <Menu>
       <MenuButton className="flex items-center gap-2">
-        <span className="text-sm font-semibold">User Name</span>
+        <span className="text-dark/50">User Name</span>
+        <Avatar name="User Name" />
       </MenuButton>
       <MenuItems
-        className="max-w-96 gap-4 rounded-2xl border border-black/10 bg-[#F7F7F7]/50 p-5 shadow-[0px_0px_30px_0px] shadow-black/5 backdrop-blur-xl"
+        as={Card}
+        // className="max-w-96 gap-4 rounded-2xl border border-black/10 bg-[#F7F7F7]/50 p-5 shadow-[0px_0px_30px_0px] shadow-black/5 backdrop-blur-xl"
+        className="mt-2 max-w-80"
         anchor="bottom"
       >
         <MenuItem as="div" className="flex items-center gap-2">
-          <Avatar className="size-10" name="teste a" />
-          Teste eee
+          <Avatar className="size-10" name="User Name" />
+          User Name
         </MenuItem>
         <div className="my-2 border-b border-black/10" />
         <div className="flex flex-col gap-2">
           <MenuItem as="div" className="flex items-center gap-2">
-            <span className="material-symbols-outlined">settings</span>
+            <span className="material-symbols-outlined text-2xl">settings</span>
             Settings
           </MenuItem>
           <MenuItem as="div" className="text-danger flex items-center gap-2">
-            <span className="material-symbols-outlined">logout</span>
-            Logout
+            <span className="material-symbols-outlined text-2xl">logout</span>
+            Sign out
           </MenuItem>
         </div>
       </MenuItems>
