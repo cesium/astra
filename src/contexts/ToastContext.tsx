792c512a6eaa77bba79dd9224e69119243dd141f
jojocoelho <jhcoelho03@gmail.com>
2025-08-16 16:06:29 +0100

    feat: toastify notifications

---
+++ src/contexts/ToastContext.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import React, { createContext, useContext, ReactNode } from 'react';
+import { toast, ToastOptions } from 'react-toastify';
+
+const defaultOptions: ToastOptions = {
+  position: "top-right",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+};
+
+interface ToastContextType {
+  showSuccess: (message: string, options?: ToastOptions) => void;
+  showError: (message: string, options?: ToastOptions) => void;
+  showWarning: (message: string, options?: ToastOptions) => void;
+  showInfo: (message: string, options?: ToastOptions) => void;
+}
+
+const ToastContext = createContext<ToastContextType | undefined>(undefined);
+
+interface ToastProviderProps {
+  children: ReactNode;
+}
+
+export const ToastProvider: React.FC<ToastProviderProps> = ({ children }) => {
+  const showSuccess = (message: string, options?: ToastOptions) => {
+    toast.success(message, { ...defaultOptions, ...options });
+  };
+
+  const showError = (message: string, options?: ToastOptions) => {
+    toast.error(message, { ...defaultOptions, ...options });
+  };
+
+  const showWarning = (message: string, options?: ToastOptions) => {
+    toast.warning(message, { ...defaultOptions, ...options });
+  };
+
+  const showInfo = (message: string, options?: ToastOptions) => {
+    toast.info(message, { ...defaultOptions, ...options });
+  };
+
+  const value = {
+    showSuccess,
+    showError,
+    showWarning,
+    showInfo,
+  };
+
+  return <ToastContext.Provider value={value}>{children}</ToastContext.Provider>;
+};
+
+export const useToast = (): ToastContextType => {
+  const context = useContext(ToastContext);
+  if (context === undefined) {
+    throw new Error('useToast must be used within a ToastProvider');
+  }
+  return context;
+};
