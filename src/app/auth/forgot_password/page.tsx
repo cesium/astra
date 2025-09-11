1be64e6de4eb97b04d42a4ec1d1a4cdee89bc930
jojocoelho <jhcoelho03@gmail.com>
2025-09-12 00:02:22 +0100

    refactor: forms successful submission in forgot password page

--- src/app/auth/forgot_password/page.tsx
+++ src/app/auth/forgot_password/page.tsx
@@ -40,37 +40,19 @@ export default function ResetPassword() {
         />
       </div>
       <div className="bg-light ring-smoke mx-4 flex flex-col gap-2 rounded-3xl p-4 ring-4 sm:mx-32 sm:min-w-md sm:gap-8 sm:p-8">
-        <div className="flex flex-col">
-          <Link
-            href="/auth/sign_in"
-            className="text-primary-400 hover:text-primary-500 flex w-fit items-center gap-2"
-          >
-            <span className="material-symbols-outlined">arrow_back</span>
-            <span>Back</span>
-          </Link>
-          <div className="flex justify-center">
-            <span className="material-symbols-outlined text-primary-400 text-6xl">
-              key
-            </span>
-          </div>
-        </div>
-        <div className="flex w-full flex-col gap-0.5 sm:gap-1.5">
-          <h1 className="text-3xl font-bold sm:text-4xl">Forgot password</h1>
-          <div className="flex flex-col">
-            <span className="max-w-72 text-gray-400">
-              Did you forget your password?
-            </span>
-            <span className="text-gray-400">
-              Try entering your email here to reset your password
-            </span>
-          </div>
-        </div>
         {forgotPassword.isSuccess ? (
           <>
-            <div className="flex flex-col items-center gap-4 text-center">
+            <div className="flex justify-center">
               <span className="material-symbols-outlined text-primary-400 text-6xl">
                 check_circle
               </span>
+            </div>
+            <div className="flex w-full flex-col gap-0.5 text-center sm:gap-1.5">
+              <h1 className="text-3xl font-bold sm:text-4xl">
+                Check your email
+              </h1>
+            </div>
+            <div className="flex flex-col items-center gap-4 text-center">
               <p className="max-w-sm text-gray-500">
                 If your account exists, an email was sent to your inbox with the
                 link to reset your password.
@@ -84,35 +66,66 @@ export default function ResetPassword() {
             </Link>
           </>
         ) : (
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col gap-9"
-          >
-            <div className="flex flex-col gap-1">
-              <Label htmlFor="email" className="text-dark pl-2 font-semibold">
-                Email
-              </Label>
-              <Input
-                {...register("email", { required: true })}
-                className="bg-dark/5 border-0 placeholder:text-black/50"
-                id="email"
-                placeholder="Email"
-              />
-              <span className="text-danger px-1">{errors.email?.message}</span>
-            </div>
-            <div className="flex flex-col gap-1 sm:gap-2">
-              <span className="text-danger px-1 text-center">
-                {forgotPassword.isError &&
-                  "Something went wrong, please try again."}
-              </span>
-              <button
-                className="bg-primary-400 mx-7 rounded-full p-4 font-bold text-white shadow-lg"
-                type="submit"
+          <>
+            <div className="flex flex-col">
+              <Link
+                href="/auth/sign_in"
+                className="text-primary-400 hover:text-primary-500 flex w-fit items-center gap-2"
               >
-                {forgotPassword.isPending ? "Loading..." : "Submit"}
-              </button>
+                <span className="material-symbols-outlined">arrow_back</span>
+                <span>Back</span>
+              </Link>
+              <div className="flex justify-center">
+                <span className="material-symbols-outlined text-primary-400 text-6xl">
+                  key
+                </span>
+              </div>
             </div>
-          </form>
+            <div className="flex w-full flex-col gap-0.5 sm:gap-1.5">
+              <h1 className="text-3xl font-bold sm:text-4xl">
+                Forgot password
+              </h1>
+              <div className="flex flex-col">
+                <span className="max-w-72 text-gray-400">
+                  Did you forget your password?
+                </span>
+                <span className="text-gray-400">
+                  Try entering your email here to reset your password
+                </span>
+              </div>
+            </div>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col gap-9"
+            >
+              <div className="flex flex-col gap-1">
+                <Label htmlFor="email" className="text-dark pl-2 font-semibold">
+                  Email
+                </Label>
+                <Input
+                  {...register("email", { required: true })}
+                  className="bg-dark/5 border-0 placeholder:text-black/50"
+                  id="email"
+                  placeholder="Email"
+                />
+                <span className="text-danger px-1">
+                  {errors.email?.message}
+                </span>
+              </div>
+              <div className="flex flex-col gap-1 sm:gap-2">
+                <span className="text-danger px-1 text-center">
+                  {forgotPassword.isError &&
+                    "Something went wrong, please try again."}
+                </span>
+                <button
+                  className="bg-primary-400 mx-7 cursor-pointer rounded-full p-4 font-bold text-white shadow-lg"
+                  type="submit"
+                >
+                  {forgotPassword.isPending ? "Loading..." : "Submit"}
+                </button>
+              </div>
+            </form>
+          </>
         )}
       </div>
     </div>
