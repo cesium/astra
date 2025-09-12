72e334a1ccacf6ff5edc1017d8f9ebb00fe2883f
Nuno Miguel <30806591+nunom27@users.noreply.github.com>
2025-09-12 16:01:03 +0100

    fix: first auth check not working (#83)

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,3 +1,4 @@
+import { AuthCheck } from "@/components/auth-check";
 import Navbar from "@/components/navbar";
 
 export default function AppLayout({
@@ -6,11 +7,13 @@ export default function AppLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-dvh flex-col">
-      <Navbar />
-      <main className="min-h-0 flex-1 px-5 pt-3.5 pb-7.5 antialiased md:px-7.5">
-        {children}
-      </main>
-    </div>
+    <AuthCheck shouldBeLoggedIn>
+      <div className="flex h-dvh flex-col">
+        <Navbar />
+        <main className="min-h-0 flex-1 px-5 pt-3.5 pb-7.5 antialiased md:px-7.5">
+          {children}
+        </main>
+      </div>
+    </AuthCheck>
   );
 }
