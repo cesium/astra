cfdb419c961b8a4a85452d006d6ea1fc901011f2
RicoPleasure <esprazj.dev@gmail.com>
2025-09-14 17:11:37 +0100

    fix: layout class

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -8,7 +8,7 @@ export default function AppLayout({
 }>) {
   return (
     <AuthCheck shouldBeLoggedIn>
-      <div className="flex min-h-dvh flex-col">
+      <div className="flex h-dvh flex-col">
         <Navbar />
         <main className="min-h-0 flex-1 px-5 pt-3.5 pb-7.5 antialiased md:px-7.5">
           {children}
