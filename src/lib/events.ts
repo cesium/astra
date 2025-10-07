52c3eb006a84bc00cb3d4bcb415f1fb85065c84d
gxnca <11gg20@gmail.com>
2025-10-07 11:52:41 +0100

    fix: missing files

---
+++ src/lib/events.ts
@@ -0,0 +1,41 @@
+import { api } from "./api";
+
+export async function getEvents() {
+  try {
+    const res = await api.get("/events");
+    return res.data.events;
+  } catch {
+    throw new Error(`Failed to fetch events. Please try again later.`);
+  }
+}
+
+export async function getEventById(id: string) {
+  try {
+    const res = await api.get(`/events/${id}`);
+    return res.data.events;
+  } catch {
+    throw new Error(
+      `Failed to fetch event with id-${id}. Please try again later.`,
+    );
+  }
+}
+
+export async function getCategories() {
+  try {
+    const res = await api.get("/event_categories");
+    return res.data.categories;
+  } catch {
+    throw new Error(`Failed to fetch categories. Please try again later.`);
+  }
+}
+
+export async function getCategoryById(id: string) {
+  try {
+    const res = await api.get(`/event_categories/${id}`);
+    return res.data.categories;
+  } catch {
+    throw new Error(
+      `Failed to fetch category with id-${id}. Please try again later.`,
+    );
+  }
+}
